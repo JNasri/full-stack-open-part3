881ee29f011d942d2227ea845e0b9b044d96e39d
JNasri <91850229+JNasri@users.noreply.github.com>
2024-10-07 18:29:05 +0300

    finishing part 3b

---
+++ phonebook_v2/README.md
@@ -0,0 +1,1 @@
+https://phonebook-v2.fly.dev/
--- phonebook_v2/index.js
+++ phonebook_v2/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const morgan = require("morgan");
+// const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
+app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
-morgan.token("body", (req) => JSON.stringify(req.body));
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+// morgan.token("body", (req) => JSON.stringify(req.body));
+// app.use(
+//   morgan(":method :url :status :res[content-length] - :response-time ms :body")
+// );
 
 let persons = [
   {
@@ -96,7 +99,7 @@ app.post("/api/persons", (request, response) => {
   response.status(201).json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
